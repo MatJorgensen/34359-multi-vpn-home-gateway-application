cca1baa05886840c404ccc9d357974dfbb51afa4
Mathias Boss Jørgensen <s164151@student.dtu.com>
2020-04-12 18:30:57 +0200

    bug fix

--- mininet/example_topo.py
+++ mininet/example_topo.py
@@ -16,19 +16,22 @@ from subprocess import call
 
 class ExampleTopo(Topo):
     "Example topology from assignment description with 4 switches and 1 host."
-    def build(self, n=4):
-	# Create four switches
-	switches = [self.addSwitch('s%s' % (s + 1), cls=OVSKernelSwitch) for s in range(n)]
-	
-	# Create a host
-	host = self.addHost('h1', cls=Host, ip='10.0.0.1', mac='00:00:00:00:00:01')
+    def build(self):
+	# Create hosts
+	h1 = self.addHost('h1', cls=Host, ip='10.0.0.1', mac='00:00:00:00:00:01')
 
-	# Add links between switches and the host
-	self.addLink(host, switches[0], cls=TCLink, **h1s1)
-	self.addLink(switches[0], switches[2], cls=TCLink, **s1s3)
-	self.addLink(switches[0], switches[3], cls=TCLink, **s1s4)
-	self.addLink(switches[1], switches[2], cls=TCLink, **s2s3)
-	self.addLink(switches[1], switches[3], cls=TCLink, **s2s4)
+	# Create switches
+	s1 = self.addSwitch('s1', cls=OVSKernelSwitch)
+	s2 = self.addSwitch('s2', cls=OVSKernelSwitch)
+	s3 = self.addSwitch('s3', cls=OVSKernelSwitch)
+	s4 = self.addSwitch('s4', cls=OVSKernelSwitch)
+
+	# Create links
+	net.addLink(h1, s1, cls=TCLink)
+	net.addLink(s1, s3, cls=TCLink)
+	net.addLink(s1, s4, cls=TCLink)
+	net.addLink(s2, s3, cls=TCLink)
+	net.addLink(s2, s4, cls=TCLink)
 
 class ONOSController(RemoteController):
     "ONOS Controller"
@@ -40,24 +43,7 @@ class ONOSController(RemoteController):
 
 topos = {'exampletopo': ExampleTopo}
 controllers = {'onos': ONOSController}
-"""
-def simpleTest():
-    "Create and test the example network"
-    topo = ExampleTopo()
-    net = Mininet(topo=topo,
-		  controller=lambda name: RemoteController(name,
-							   ip='127.0.0.1',
-							   protocol='tcp',
-							   port=6653),
-		  ipBase='10.0.0.0/8')
 
-    net.start()
-    print "Dumping host connections"
-    dumpNodeConnections(net.hosts)
-    print "Testing network connectivity"
-    CLI(net)
-    net.stop()
-"""
 if __name__ == '__main__':
     net.start()
     CLI(net)
