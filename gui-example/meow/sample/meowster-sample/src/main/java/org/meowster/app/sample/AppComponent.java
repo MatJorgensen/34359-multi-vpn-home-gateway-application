50485930397cef268cb316796fabdb17a3218a99
frander1 <frederik396@gmail.com>
2020-05-04 22:00:07 +0200

    added gui to change VLAN

---
+++ gui-example/meow/sample/meowster-sample/src/main/java/org/meowster/app/sample/AppComponent.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright 2020-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.meowster.app.sample;
+
+import org.apache.felix.scr.annotations.*;
+import org.onlab.packet.Ethernet;
+import org.onlab.packet.MacAddress;
+import org.onlab.packet.VlanId;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.CoreService;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.flow.*;
+import org.onosproject.net.flowobjective.DefaultForwardingObjective;
+import org.onosproject.net.flowobjective.FlowObjectiveService;
+import org.onosproject.net.flowobjective.ForwardingObjective;
+import org.onosproject.net.packet.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Skeletal ONOS application component.
+ */
+@Component(immediate = true)
+public class AppComponent {
+    private ApplicationId appId;
+    private static String H1_MAC = "00:00:00:00:00:01";
+    private static String H2_MAC = "00:00:00:00:00:02";
+    private static String S1_ID = "of:0000000000000001";
+    private static String S2_ID = "of:0000000000000002";
+    private static long H1_S1_PORT = 1;
+    private static long H2_S2_PORT = 1;
+    private static long S1_S2_PORT = 2;
+    private static long S2_S1_PORT = 2;
+    //static boolean VlanOn = false;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected CoreService coreService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    FlowObjectiveService flowObjectiveService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected FlowRuleService flowRuleService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected PacketService packetService;
+
+    private ReactivePacketProcessor processor = new ReactivePacketProcessor();
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Activate
+    protected void activate() {
+        appId = coreService.registerApplication("org.student.multi-vpn-app");
+        TrafficSelector packetSelector = DefaultTrafficSelector.builder()
+                .matchEthType(Ethernet.TYPE_IPV4).build();
+        packetService.requestPackets(packetSelector, PacketPriority.REACTIVE, appId);
+        packetService.addProcessor(processor, PacketProcessor.director(2));
+        log.info("Started");
+    }
+
+    @Deactivate
+    protected void deactivate() {
+        flowRuleService.removeFlowRulesById(appId);
+        packetService.removeProcessor(processor);
+        processor = null;
+        log.info("Stopped");
+    }
+
+    private class ReactivePacketProcessor implements PacketProcessor {
+        @Override
+        public void process(PacketContext context) {
+            InboundPacket pkt = context.inPacket();
+            Ethernet ethPkt = pkt.parsed();
+            //Discard if packet is null.
+            if (ethPkt == null) {
+                log.info("Discarding null packet");
+                return;
+            }
+
+            if (ethPkt.getEtherType() != Ethernet.TYPE_IPV4) {
+                return;
+            }
+
+            log.info("Processing packet request");
+            // log.info("VlanID: " + ethPkt.getVlanID());
+
+            DeviceId deviceId = pkt.receivedFrom().deviceId();
+            MacAddress dstMac = ethPkt.getDestinationMAC();
+            PortNumber outPort = PortNumber.FLOOD;
+            VlanId vlanId = VlanId.vlanId(VlanId.UNTAGGED);
+
+            TrafficSelector.Builder packetSelector1 = DefaultTrafficSelector.builder();
+            packetSelector1.matchEthType(Ethernet.TYPE_IPV4);
+
+            TrafficSelector.Builder packetSelector2 = DefaultTrafficSelector.builder();
+            packetSelector2.matchEthType(Ethernet.TYPE_IPV4);
+
+            TrafficTreatment.Builder packetTreatment1 = DefaultTrafficTreatment.builder();
+            TrafficTreatment.Builder packetTreatment2 = DefaultTrafficTreatment.builder();
+
+            if (deviceId.equals(DeviceId.deviceId("of:0000000000000001"))) {
+                if (dstMac.equals(MacAddress.valueOf("00:00:00:00:00:01"))) {
+                    outPort = PortNumber.portNumber(1);
+                    vlanId = VlanId.vlanId(vlanId.UNTAGGED);
+                    log.info("Setting output port to: " + outPort);
+                    log.info("Tagging packet with vlan tag " + vlanId);
+                    packetSelector1.matchVlanId(VlanId.vlanId((short) 200));
+                    packetSelector1.matchEthDst(dstMac);
+                    packetTreatment1.popVlan();
+                    packetTreatment1.setOutput(outPort);
+                    forwardRequest(context, packetSelector1, packetTreatment1, deviceId, outPort);
+
+                } else if (dstMac.equals(MacAddress.valueOf("00:00:00:00:00:02"))) {
+                    outPort = PortNumber.portNumber(2);
+                    vlanId = VlanId.vlanId((short) 200);
+                    log.info("Setting output port to: " + outPort);
+                    log.info("Tagging packet with vlan tag " + vlanId);
+                    packetSelector1.matchEthDst(dstMac);
+                    if (AppUiMessageHandler.getVlan()) {
+                        packetTreatment1.pushVlan().setVlanId(vlanId).setOutput(outPort);
+                        log.info("S1, destination = h2, PUSHING VLAN TAG");
+                    }else{
+                        packetTreatment1.setOutput(outPort);
+                        log.info("S1, destination = h2, NOT PUSHING VLAN TAG");
+                    }
+                    forwardRequest(context, packetSelector1, packetTreatment1, deviceId, outPort);
+                } else {
+                    log.info("Unknown destination host, ignoring");
+                    return;
+                }
+            } else if (deviceId.equals(DeviceId.deviceId("of:0000000000000002"))) {
+                log.info("s2");
+                if (dstMac.equals(MacAddress.valueOf("00:00:00:00:00:01"))) {
+                    outPort = PortNumber.portNumber(2);
+                    vlanId = VlanId.vlanId((short) 200);
+                    log.info("Setting output port to: " + outPort);
+                    log.info("Tagging packet with vlan tag " + vlanId);
+                    packetSelector2.matchEthDst(dstMac);
+                    if (AppUiMessageHandler.getVlan()) {
+                        packetTreatment2.pushVlan().setVlanId(vlanId).setOutput(outPort);
+                    }else{
+                        packetTreatment2.setOutput(outPort);
+                    }
+                    forwardRequest(context, packetSelector2, packetTreatment2, deviceId, outPort);
+                } else if (dstMac.equals(MacAddress.valueOf("00:00:00:00:00:02"))) {
+                    outPort = PortNumber.portNumber(1);
+                    vlanId = VlanId.vlanId(VlanId.UNTAGGED);
+                    log.info("Setting output port to: " + outPort);
+                    log.info("Tagging packet with vlan tag " + vlanId);
+                    packetSelector2.matchVlanId(VlanId.vlanId((short) 200));
+                    packetSelector2.matchEthDst(dstMac);
+                    packetTreatment2.popVlan();
+                    packetTreatment2.setOutput(outPort);
+                    forwardRequest(context, packetSelector2, packetTreatment2, deviceId, outPort);
+                } else {
+                    log.info("Unknown destination host, ignoring");
+                    return;
+                }
+            }
+            //packetSelector1.matchEthDst(dstMac);
+
+            //if (outPort != PortNumber.FLOOD) flowObjectiveService.forward(deviceId, forwardingObjective);
+            // context.treatmentBuilder().addTreatment(treatment.build());
+            //packetTreatment.setOutput(outPort);
+
+            //Generate the traffic selector based on the packet that arrived.
+            //TrafficSelector packetSelector = DefaultTrafficSelector.builder()
+            //        .matchEthType(Ethernet.TYPE_IPV4)
+            //        .matchVlanId(vlanId)
+            //        .matchEthDst(dstMac).build();
+
+            //TrafficTreatment treatment = DefaultTrafficTreatment.builder()
+            //        .pushVlan()
+            //        .setVlanId(vlanId)
+            //        .setOutput(outPort).build();
+
+            //ForwardingObjective forwardingObjective = DefaultForwardingObjective.builder()
+            //        .withSelector(packetSelector.build())
+            //        .withTreatment(packetTreatment.build())
+            //        .withPriority(5000)
+            //        .withFlag(ForwardingObjective.Flag.VERSATILE)
+            //        .fromApp(appId)
+            //        .makeTemporary(5)
+            //        .add();
+
+            //if (outPort != PortNumber.FLOOD) flowObjectiveService.forward(deviceId, forwardingObjective);
+            //context.treatmentBuilder().addTreatment(packetTreatment.build());
+            //context.send();
+
+        }
+
+        public void forwardRequest(PacketContext context, TrafficSelector.Builder selector, TrafficTreatment.Builder treatment, DeviceId deviceId, PortNumber outPort) {
+            ForwardingObjective forwardingObjective = DefaultForwardingObjective.builder()
+                    .withSelector(selector.build())
+                    .withTreatment(treatment.build())
+                    .withPriority(5000)
+                    .withFlag(ForwardingObjective.Flag.VERSATILE)
+                    .fromApp(appId)
+                    .makeTemporary(5)
+                    .add();
+
+            if (outPort != PortNumber.FLOOD) flowObjectiveService.forward(deviceId, forwardingObjective);
+            context.treatmentBuilder().addTreatment(treatment.build());
+            context.send(); // TJEK DENNE...!
+        }
+    }
+}
---
+++ gui-example/meow/sample/meowster-sample/src/main/java/org/meowster/app/sample/AppUiMessageHandler.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2020-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.meowster.app.sample;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableSet;
+import org.onosproject.ui.RequestHandler;
+import org.onosproject.ui.UiMessageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+
+/**
+ * Skeletal ONOS UI Custom-View message handler.
+ */
+
+public class AppUiMessageHandler extends UiMessageHandler {
+
+    private static final String SAMPLE_CUSTOM_DATA_REQ = "sampleCustomDataRequest";
+    private static final String SAMPLE_CUSTOM_DATA_RESP = "sampleCustomDataResponse";
+
+    private static final String Customer1VLAN = "Customer1VLAN";
+    private static final String Customer2VLAN = "Customer2VLAN";
+    private static final String Customer3VLAN = "Customer3VLAN";
+    private static final int C1VLAN = 100;
+    private static final int C2VLAN = 200;
+    private static final int C3VLAN = 300;
+    private static final int NoVlan = 0;
+    private static final String MESSAGE = "message";
+    private static final String MSG_FORMAT = "VLAN TAG VALUE IS: %d ";
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private static boolean VlanOn = false;
+
+    public static boolean getVlan() {
+        return VlanOn;
+    }
+    public static int getC1vlan() {
+        return C1VLAN;
+    }
+    public static int getC2vlan() {
+        return C2VLAN;
+    }
+    public static int getC3vlan() {
+        return C3VLAN;
+    }
+
+    @Override
+    protected Collection<RequestHandler> createRequestHandlers() {
+        return ImmutableSet.of(
+                new SampleCustomDataRequestHandler()
+        );
+    }
+
+    // handler for sample data requests
+    private final class SampleCustomDataRequestHandler extends RequestHandler {
+
+        private SampleCustomDataRequestHandler() {
+            super(SAMPLE_CUSTOM_DATA_REQ);
+        }
+
+        @Override
+        public void process(ObjectNode payload) {
+            if(!VlanOn){
+                VlanOn = true;
+                log.debug("Computing data for {}...", 444);
+
+                ObjectNode result = objectNode();
+                result.put(Customer1VLAN, C1VLAN);
+                result.put(Customer2VLAN, C2VLAN);
+                result.put(Customer3VLAN, C3VLAN);
+                result.put(MESSAGE, String.format(MSG_FORMAT, 1));
+                sendMessage(SAMPLE_CUSTOM_DATA_RESP, result);
+
+            }
+            else{
+                VlanOn = false;
+                ObjectNode result = objectNode();
+                result.put(Customer1VLAN, NoVlan);
+                result.put(Customer2VLAN, NoVlan);
+                result.put(Customer3VLAN, NoVlan);
+                result.put(MESSAGE, String.format(MSG_FORMAT, 0));
+                sendMessage(SAMPLE_CUSTOM_DATA_RESP, result);
+            }
+        }
+    }
+}
